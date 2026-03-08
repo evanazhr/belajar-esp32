328414a69e15a075998166ac5994bc833d53c2a2
evanazh <evanazhr@gmail.com>
2026-03-09 03:05:09 +0700

    feat : add API post request limiter

--- src/app/api/sensors/route.ts
+++ src/app/api/sensors/route.ts
@@ -2,35 +2,56 @@ export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
+import { LRUCache } from "lru-cache";
 
-// initialization Supabase
+const tokenCache = new LRUCache({
+  max: 500, // Maksimal 500 IP unik yang dilacak
+  ttl: 60 * 1000, // Time to live: 60 detik
+});
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabasePublishableKey =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
-const supabase = createClient(supabaseUrl, supabasePublishableKey);
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function POST(request: Request) {
   try {
+    // Ambil IP Address pengirim buat jadi pengenal
+    const ip = request.headers.get("x-forwarded-for") || "anonymous";
+    
+    // Cek jumlah request dari IP ini
+    const tokenCount = (tokenCache.get(ip) as number[]) || [0];
+    if (tokenCount[0] === 0) {
+      tokenCache.set(ip, [1]);
+    } else {
+      tokenCount[0] += 1;
+      tokenCache.set(ip, tokenCount);
+    }
+
+    // Batasan 20 request per menit
+    if (tokenCount[0] > 20) {
+      return NextResponse.json(
+        { error: "Too many requests" },
+        { status: 429 }
+      );
+    }
+
     const { device_id, temperature, humidity, moisture, pump } = await request.json();
 
-    // insert data to sensor_log table
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from("sensor_log")
       .insert([{ device_id, temperature, humidity, moisture, pump }]);
 
     if (error) throw error;
 
     return NextResponse.json(
-      { message: "Data tersimpan di Supabase!" },
+      { message: "Data tersimpan!" },
       {
         status: 200,
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Access-Control-Allow-Methods": "POST, OPTIONS",
-          "Access-Control-Allow-Headers":
-            "Content-Type, Authorization, ngrok-skip-browser-warning",
         },
-      },
+      }
     );
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
